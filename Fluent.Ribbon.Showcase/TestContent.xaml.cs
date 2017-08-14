d1318f0d094de334027ad60dd6a13eeb9385f628
Cubey2019 <Cubey2019@users.noreply.github.com>
2017-08-14 16:51:57 +0100

    Changes to the TestWindow and TestContent XAML to demonstrate issue with the SizeToContent issue.

--- Fluent.Ribbon.Showcase/TestContent.xaml.cs
+++ Fluent.Ribbon.Showcase/TestContent.xaml.cs
@@ -210,7 +210,7 @@ namespace FluentTest
 
         private void OnHelpClick(object sender, RoutedEventArgs e)
         {
-            if (this.tabGroup1.Visibility == Visibility.Visible)
+            /*if (this.tabGroup1.Visibility == Visibility.Visible)
             {
                 this.tabGroup1.Visibility = Visibility.Collapsed;
                 this.tabGroup2.Visibility = Visibility.Collapsed;
@@ -219,7 +219,7 @@ namespace FluentTest
             {
                 this.tabGroup1.Visibility = Visibility.Visible;
                 this.tabGroup2.Visibility = Visibility.Visible;
-            }
+            }*/
         }
 
         private void OnSpinnerValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
